95033da338a5b30d6933afb324363cf0d6d9b245
Christian Matthias <xtianus@live.com>
2018-03-05 10:53:22 -0500

    updated PushNotificationJS file and updated ios PushPluginM file to handle webview

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -218,7 +218,7 @@
         NSLog(@"Msg: %@", jsonStr);
 
         NSString * jsCallBack = [NSString stringWithFormat:@"%@(%@);", self.callback, jsonStr];
-        [self.webView stringByEvaluatingJavaScriptFromString:jsCallBack];
+        [(UIWebView*)self.webView stringByEvaluatingJavaScriptFromString:jsCallBack];
 
         self.notificationMessage = nil;
     }
