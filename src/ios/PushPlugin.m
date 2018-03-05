ef15bf9dbacc12108283285ba3d4664f197e0152
Christian Matthias <xtianus@live.com>
2018-03-05 11:04:31 -0500

    udpated pluginXML to reflect 2018 changes

--- src/ios/PushPlugin.m
+++ src/ios/PushPlugin.m
@@ -1,5 +1,7 @@
 /*
- Copyright 2009-2011 Urban Airship Inc. All rights reserved.
+ Copyright 2009-2018 Urban Airship Inc. All rights reserved.
+
+ Updated for ios in 2018
 
  Redistribution and use in source and binary forms, with or without
  modification, are permitted provided that the following conditions are met:
